c04ceb7574c34ec685bf15273cb7293acb0bb0ee
Atypunj <atypunjairdrop1@gmail.com>
2025-05-05 02:58:52 +0530

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,16 @@
-document.getElementById("root").innerHTML += "<p>JS Loaded ✅</p>";
+document.getElementById("birthForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("fullName").value;
+  const dob = document.getElementById("dob").value;
+  const time = document.getElementById("birthTime").value;
+  const place = document.getElementById("birthPlace").value;
+
+  document.getElementById("result").innerHTML = `
+    <h3>Submitted Details</h3>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Date of Birth:</strong> ${dob}</p>
+    <p><strong>Time of Birth:</strong> ${time}</p>
+    <p><strong>Place of Birth:</strong> ${place}</p>
+  `;
+});
